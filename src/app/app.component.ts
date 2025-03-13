5f623b09df81cc1829fb61a59b982aed69e3890c
dollyaggarwal <dolly.aggarwal@oodles.io>
2025-03-13 11:29:50 +0530

    routerlink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LoginformComponent } from './loginform/loginform.component';
+import { Router, RouterOutlet } from '@angular/router';
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, LoginformComponent],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
+    constructor(public router: Router) {
+        this.router.events.subscribe(event => {
+            console.log('Router event:', event);
+        });
+    }
 }
