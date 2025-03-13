3d14e6b9d69cd534d526b0f14c198a6fcef9fc13
dollyaggarwal <dolly.aggarwal@oodles.io>
2025-03-13 17:34:59 +0530

    dummy page designed to show dynamic route name

--- src/app/dummy/dummy.component.ts
+++ src/app/dummy/dummy.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-dummy',
-  standalone: true,
-  imports: [],
-  templateUrl: './dummy.component.html',
-  styleUrl: './dummy.component.css'
+    selector: 'app-dummy',
+    standalone: true,
+    imports: [],
+    templateUrl: './dummy.component.html',
+    styleUrl: './dummy.component.css'
 })
 export class DummyComponent {
+    pageTitle: string = '';
+
+    constructor(public route: ActivatedRoute) { }
+
+    ngOnInit() {
+        // Get the last segment of the route and format it
+        this.route.url.subscribe(urlSegments => {
+            if (urlSegments.length > 0) {
+                this.pageTitle = urlSegments[urlSegments.length - 1].path.replace(/-/g, ' ');
+            }
+        });
+    }
 
 }
